53f2bdb326a3df69e3f58b36462c1d4de233040b
QuanNguyen <gideonnguyen94@gmail.com>
2025-05-20 23:07:19 +0700

    chore: update env

--- backend/src/config/data-source.ts
+++ backend/src/config/data-source.ts
@@ -1,12 +1,17 @@
 import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: process.env.NODE_ENV === "production" ? ".env" : "local.env",
+});
 
 export const PostgresDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "dbAnyMindTest",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "5432"),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE || "dbAnyMindTest",
   entities: [__dirname + "/../entities/**/*.ts"],
   synchronize: true,
 });
