5e503194e0ccd0c1e2c391aa6bda9597e01d5e08
QuanNguyen <gideonnguyen94@gmail.com>
2025-05-21 09:18:35 +0700

    feat: api rating logic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,13 +5,25 @@ import routes from "@/routes";
 import { PostgresDataSource } from "@config/data-source";
 import cors from "cors";
 import express from "express";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 5, // 5 requests per minute
+  message: {
+    error: "Too many requests, please try again later.",
+  },
+  headers: true, // Include rate limit info in response headers
+});
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Apply the limiter to all requests
+app.use(limiter);
 
 // Initial connect database
 PostgresDataSource.initialize().then(() => {
